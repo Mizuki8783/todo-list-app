c9d45517fa1a991604b4d1f2f1c43c3a02189144
tyoshilab <aiko.jitsui@gmail.com>
2025-06-26 20:59:21 +0000

    fix: task.deadline always used as date type

--- Task.py
+++ Task.py
@@ -1,11 +1,15 @@
+from datetime import date
+from typing import Optional
+
 class Task:
-    def __init__(self, name: str, deadline: int = 0, priority: int = 0):
+    def __init__(self, name: str, deadline: Optional[date] = None, priority: int = 0):
         self.name = name
         self.priority = priority
         self.deadline = deadline
 
     def __str__(self):
-        return f"{self.name} - {self.priority} - {self.deadline}"
+        deadline_str = self.deadline.strftime('%Y-%m-%d') if self.deadline else "N/A"
+        return f"Name: {self.name}, Priority: {self.priority}, Deadline: {deadline_str}"
 
     def __repr__(self):
         return self.__str__()
